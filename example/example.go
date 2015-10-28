26688e88873a6cc19d69f85855ab5c1ca436a715
Felipe Cavani <fcavani@gmail.com>
2015-10-28 13:16:04 -0200

    example don't work in my machine

--- example/example.go
+++ example/example.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"errors"
 	"log"
 	"net"
 
 	"github.com/fcavani/discover"
+	"github.com/fcavani/e"
 )
 
 func main() {
@@ -13,15 +13,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Interface:", in)
 
 	server := &discover.Server{}
 	server.Interface = in
 	server.AddrVer = discover.Ipv4
-	server.Protocol = func(addr *net.UDPAddr, recv []byte) (msg []byte, err error) {
-		if string(recv) != "request" {
-			return nil, errors.New("protocol error")
+	server.Protocol = func(addr *net.UDPAddr, req *discover.Request) (resp *discover.Response, err error) {
+		if string(req.Data) != "request" {
+			return nil, e.New("protocol error")
 		}
-		return []byte("msg"), nil
+		return &discover.Response{
+			Data: []byte("msg"),
+		}, nil
 	}
 	err = server.Do()
 	if err != nil {
@@ -32,12 +35,14 @@ func main() {
 	client := &discover.Client{}
 	client.Interface = in
 	client.AddrVer = discover.Ipv4
-	client.Request = func(dst *net.UDPAddr) ([]byte, error) {
-		return []byte("request"), nil
+	client.Request = func(dst *net.UDPAddr) (*discover.Request, error) {
+		return &discover.Request{
+			Data: []byte("request"),
+		}, nil
 	}
-	buf, err := client.Discover()
+	resp, err := client.Discover()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(string(buf))
+	log.Println(resp)
 }
