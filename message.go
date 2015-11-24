7d424eb31de4776a70b92ea1d98f7bb6586fb5c3
Felipe Cavani <fcavani@gmail.com>
2015-11-24 10:34:00 -0200

    encrypt big messages

--- message.go
+++ message.go
@@ -15,49 +15,70 @@ import (
 type Msg struct {
 	From      string
 	To        string
-	Data      []byte
-	Signature []byte
+	Data      [][]byte
+	Signature [][]byte
 	Err       error
 }
 
 func NewMsg(from, to string, fromkey *rsa.PrivateKey, tokey *rsa.PublicKey, data []byte) (*Msg, error) {
 	hash := crypto.SHA256
-
-	ciphertext, err := rsa.EncryptOAEP(hash.New(), rand.Reader, tokey, data, []byte(""))
-	if err != nil {
-		return nil, e.Push(err, "can't encrypt message")
+	max := tokey.N.BitLen()/8 - 2*hash.Size() - 2
+	num := len(data) / max
+	if len(data)%max > 0 {
+		num++
 	}
-	pssh := hash.New()
-	pssh.Write(data)
-	hashed := pssh.Sum(nil)
-	var opts rsa.PSSOptions
-	opts.SaltLength = rsa.PSSSaltLengthAuto
-	signature, err := rsa.SignPSS(rand.Reader, fromkey, hash, hashed, &opts)
-	if err != nil {
-		return nil, e.Push(err, "can't sign the message")
-	}
-	return &Msg{
+	msg := &Msg{
 		From:      from,
 		To:        to,
-		Data:      ciphertext,
-		Signature: signature,
-	}, nil
+		Data:      make([][]byte, num),
+		Signature: make([][]byte, num),
+	}
+	j := 0
+	for i := 0; i < len(data); i += max {
+		end := i + max
+		if end > len(data) {
+			end = len(data)
+		}
+
+		ciphertex, err := rsa.EncryptOAEP(hash.New(), rand.Reader, tokey, data[i:end], []byte(""))
+		if err != nil {
+			return nil, e.Push(err, "can't encrypt message")
+		}
+		pssh := hash.New()
+		pssh.Write(data[i:end])
+		hashed := pssh.Sum(nil)
+		var opts rsa.PSSOptions
+		opts.SaltLength = rsa.PSSSaltLengthAuto
+		signature, err := rsa.SignPSS(rand.Reader, fromkey, hash, hashed, &opts)
+		if err != nil {
+			return nil, e.Push(err, "can't sign the message")
+		}
+		msg.Data[j] = ciphertex
+		msg.Signature[j] = signature
+		j++
+	}
+
+	return msg, nil
 }
 
-func (m *Msg) Message(fromkey *rsa.PublicKey, dstkey *rsa.PrivateKey) ([]byte, error) {
+func (m *Msg) Message(fromkey *rsa.PublicKey, dstkey *rsa.PrivateKey) (data []byte, err error) {
 	hash := crypto.SHA256
-	plainText, err := rsa.DecryptOAEP(hash.New(), rand.Reader, dstkey, m.Data, []byte(""))
-	if err != nil {
-		return nil, e.Push(err, "can't decrypt the message")
+	data = make([]byte, 0)
+	for i, d := range m.Data {
+		plainText, err := rsa.DecryptOAEP(hash.New(), rand.Reader, dstkey, d, []byte(""))
+		if err != nil {
+			return nil, e.Push(err, "can't decrypt the message")
+		}
+		pssh := hash.New()
+		pssh.Write(plainText)
+		hashed := pssh.Sum(nil)
+		var opts rsa.PSSOptions
+		opts.SaltLength = rsa.PSSSaltLengthAuto
+		err = rsa.VerifyPSS(fromkey, hash, hashed, m.Signature[i], &opts)
+		if err != nil {
+			return nil, e.Push(err, "can't verify the signature")
+		}
+		data = append(data, plainText...)
 	}
-	pssh := hash.New()
-	pssh.Write(plainText)
-	hashed := pssh.Sum(nil)
-	var opts rsa.PSSOptions
-	opts.SaltLength = rsa.PSSSaltLengthAuto
-	err = rsa.VerifyPSS(fromkey, hash, hashed, m.Signature, &opts)
-	if err != nil {
-		return nil, e.Push(err, "can't verify the signature")
-	}
-	return plainText, nil
+	return data, nil
 }
