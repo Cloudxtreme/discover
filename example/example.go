703df1d5a7743681309b8a502f51d686fbac4594
Felipe Cavani <fcavani@gmail.com>
2015-12-05 16:24:49 -0200

    update the example

--- example/example.go
+++ example/example.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"log"
 	"net"
 
@@ -15,9 +17,25 @@ func main() {
 	}
 	log.Println("Interface:", in)
 
+	MasterKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatal(err)
+	}
+	SlaveKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Keys := discover.NewPubKeys()
+	Keys.Put("slave", &SlaveKey.PublicKey)
+
 	server := &discover.Server{}
+	server.Name = "master"
+	server.PrivateKey = MasterKey
+	server.PubKeys = Keys
 	server.Interface = in
 	server.AddrVer = discover.Ipv4
+	server.Port = "3333"
 	server.Protocol = func(addr *net.UDPAddr, req *discover.Request) (resp *discover.Response, err error) {
 		if string(req.Data) != "request" {
 			return nil, e.New("protocol error")
@@ -33,8 +51,13 @@ func main() {
 	defer server.Close()
 
 	client := &discover.Client{}
+	client.ServerName = "master"
+	client.ServerKey = &MasterKey.PublicKey
+	client.Name = "slave"
+	client.PrivateKey = SlaveKey
 	client.Interface = in
 	client.AddrVer = discover.Ipv4
+	client.Port = "3333"
 	client.Request = func(dst *net.UDPAddr) (*discover.Request, error) {
 		return &discover.Request{
 			Data: []byte("request"),
